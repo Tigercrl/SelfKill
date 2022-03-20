d5cb7d4bf3a7e275aced4441e245752e3b14405d
Tigercrl <73047129+Tigercrl@users.noreply.github.com>
2022-03-20 18:41:17 +0800

    Update SelfKill.java

--- src/main/java/me/tigercrl/selfkill/SelfKill.java
+++ src/main/java/me/tigercrl/selfkill/SelfKill.java
@@ -7,9 +7,9 @@ import org.bukkit.ChatColor;
 public final class SelfKill extends JavaPlugin {
     @Override
     public void onEnable() {
-        System.out.println(ChatColor.DARK_GREEN+"[SelfKill]插件已成功加载！");
-        System.out.println(ChatColor.DARK_GREEN+"[SelfKill]作者：Tigercrl");
-        System.out.println(ChatColor.DARK_GREEN+"[SelfKill]版本：v1.0");
+        System.out.println(ChatColor.DARK_GREEN+"[SelfKill]Plugin successfully loaded!");
+        System.out.println(ChatColor.DARK_GREEN+"[SelfKill]Author：Tigercrl");
+        System.out.println(ChatColor.DARK_GREEN+"[SelfKill]Version：1.0");
 
         getCommand("selfkill").setExecutor(new SelfKillCommand());
     }
@@ -17,7 +17,7 @@ public final class SelfKill extends JavaPlugin {
     //插件停用时执行次方法
     @Override
     public void onDisable() {
-        System.out.println(ChatColor.DARK_GREEN+"[SelfKill]插件已成功卸载！");
+        System.out.println(ChatColor.DARK_GREEN+"[SelfKill]Plugin successfully unloaded!");
     }
 
 }
